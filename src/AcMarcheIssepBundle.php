47056a7d1bc423e57738ded1f3066ef4683569bb
jfsenechal <jfsenechal@gmail.com>
2022-05-30 15:21:26 +0200

    Remove di extension

--- src/AcMarcheIssepBundle.php
+++ src/AcMarcheIssepBundle.php
@@ -2,12 +2,19 @@
 
 namespace AcMarche\Issep;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class AcMarcheIssepBundle extends Bundle
+class AcMarcheIssepBundle extends  AbstractBundle
 {
     public function getPath(): string
     {
         return \dirname(__DIR__);
     }
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/packages/twig.php');
+    }
+
 }
