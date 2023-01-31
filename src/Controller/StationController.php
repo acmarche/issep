9beace5316d13c66e39fec8bb931ba53dcdcf45f
jfsenechal <jfsenechal@gmail.com>
2023-01-31 17:00:48 +0100

    update sf 6.2

--- src/Controller/StationController.php
+++ src/Controller/StationController.php
@@ -8,14 +8,14 @@ use AcMarche\Issep\Indice\IndiceUtils;
 use AcMarche\Issep\Repository\StationRepository;
 use AcMarche\Issep\Utils\FeuUtils;
 use AcMarche\Issep\Utils\SortUtils;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(path: '/')]
-#[IsGranted(data: 'ROLE_CAPTEUR')]
+#[IsGranted('ROLE_CAPTEUR')]
 class StationController extends AbstractController
 {
     public function __construct(private StationRepository $stationRepository, private IndiceUtils $indiceUtils)
