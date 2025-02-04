0cebddf7a4d5f57461f4487587a6f9e50a04140c
jfsenechal <jfsenechal@gmail.com>
2025-02-04 09:50:31 +0100

    fix bad stations

--- src/Repository/StationsEnum.php
+++ src/Repository/StationsEnum.php
@@ -5,10 +5,10 @@ namespace AcMarche\Issep\Repository;
 enum StationsEnum: int
 {
     case CHAUSSEE_LIEGE = 15;
-    case RUE_NERETTE = 17;
+    case RUE_NERETTE = 22;
     case BRETELLE_N4 = 18;
     case BOULEVARD_NORD = 26;
-    case AVENUE_FRANCE = 27;
+    case AVENUE_FRANCE = 19;
     case CONTOURNEMENT_N839 = 30;
     case CIMETIERE_AYE = 66;
     case SINSIN = 1023;
